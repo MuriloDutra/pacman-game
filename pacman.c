ee23fcf100c3f0f1ecdca1f41c723097914b1a99
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-02-02 22:15:18 -0300

    Breaking code into functions

--- pacman.c
+++ pacman.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pacman.h"
 
 char** map;
 int lines;
 int columns;
 
 int main(){
+    read_map();
+
+    for(int i = 0; i < lines; i++){
+        printf("%s\n", map[i]);
+    }
+
+    free_map();
+}
+
+void read_map(){
     FILE* file;
     file = fopen("map.txt", "r");//"r" means "read"
     if(file == 0){
@@ -14,23 +25,23 @@ int main(){
     }
 
     fscanf(file, "%d %d", &lines, &columns);
-    printf("lines %d | columns %d\n", lines, columns);
 
-    map = malloc(sizeof(char*) * lines);
-    for(int i = 0; i < lines; i++){
-        map[i] = malloc(sizeof(char) * (columns + 1));
-    }
+    allocate_map();
 
     for(int i = 0; i < lines; i++){
         fscanf(file, "%s", map[i]);
     }
+    fclose(file); 
+}
 
+void allocate_map(){
+    map = malloc(sizeof(char*) * lines);
     for(int i = 0; i < lines; i++){
-        printf("%s\n", map[i]);
+        map[i] = malloc(sizeof(char) * (columns + 1));
     }
+}
 
-    fclose(file);
-
+void free_map(){
     for(int i = 0; i < lines; i++){
         free(map[i]);
     }
---
+++ pacman.h
@@ -0,0 +1,3 @@
+void free_map();
+void read_map();
+void allocate_map(); 

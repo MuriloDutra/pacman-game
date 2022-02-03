d2a307e4f4e0b2b312aa96b4be4cdb836f1162c8
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-02-02 21:48:39 -0300

    Using malloc to dynamically create matrices

--- pacman.c
+++ pacman.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    char map[5][10+1];
+char** map;
+int lines;
+int columns;
 
+int main(){
     FILE* file;
     file = fopen("map.txt", "r");//"r" means "read"
-
     if(file == 0){
         printf("Game map not found.\n");
         exit(1);
     }
 
-    for(int i = 0; i < 5; i++){
+    fscanf(file, "%d %d", &lines, &columns);
+    printf("lines %d | columns %d\n", lines, columns);
+
+    map = malloc(sizeof(char*) * lines);
+    for(int i = 0; i < lines; i++){
+        map[i] = malloc(sizeof(char) * (columns + 1));
+    }
+
+    for(int i = 0; i < lines; i++){
         fscanf(file, "%s", map[i]);
     }
 
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < lines; i++){
         printf("%s\n", map[i]);
     }
 
-    fclose(file); 
+    fclose(file);
+
+    for(int i = 0; i < lines; i++){
+        free(map[i]);
+    }
+    free(map);
 }

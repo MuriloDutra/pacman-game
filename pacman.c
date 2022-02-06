fde6b2298fba7ce8dd3157669c1ef8b2b1a25468
Murilo Dutra Alves <murilo.dutra2000@gmail.com>
2022-02-06 12:41:33 -0300

    Declaring a struct and creating more functions

--- pacman.c
+++ pacman.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include "pacman.h"
 
-char** map;
-int lines;
-int columns;
-
+struct map map;
+ 
 int main(){
     read_map();
-
-    for(int i = 0; i < lines; i++){
-        printf("%s\n", map[i]);
-    }
-
+    do{
+        print_map();
+        char command;
+        scanf(" %c", &command);
+        move(command);
+    }while(!game_is_over());
+    
     free_map();
 }
 
@@ -24,26 +24,69 @@ void read_map(){
         exit(1);
     }
 
-    fscanf(file, "%d %d", &lines, &columns);
-
+    fscanf(file, "%d %d", &(map.lines), &(map.columns));
     allocate_map();
 
-    for(int i = 0; i < lines; i++){
-        fscanf(file, "%s", map[i]);
+    for(int i = 0; i < map.lines; i++){
+        fscanf(file, "%s", map.matrix[i]);
     }
     fclose(file); 
 }
 
 void allocate_map(){
-    map = malloc(sizeof(char*) * lines);
-    for(int i = 0; i < lines; i++){
-        map[i] = malloc(sizeof(char) * (columns + 1));
+    map.matrix = malloc(sizeof(char*) * map.lines);
+    for(int i = 0; i < map.lines; i++){
+        map.matrix[i] = malloc(sizeof(char) * (map.columns + 1));
     }
 }
 
 void free_map(){
-    for(int i = 0; i < lines; i++){
-        free(map[i]);
+    for(int i = 0; i < map.lines; i++){
+        free(map.matrix[i]);
     }
-    free(map);
+    free(map.matrix);
+}
+
+void print_map(){
+    for(int i = 0; i < map.lines; i++){
+        printf("%s\n", map.matrix[i]);
+    }
+}
+
+int game_is_over(){
+    return 0;
+}
+
+void move(char command){
+    int line;
+    int column;
+
+    find_character(&line, &column);
+    switch(command){
+        case 'w': //UP
+            map.matrix[line - 1][column] = '@';
+            break;
+        case 'a': //LEFT
+            map.matrix[line][column - 1] = '@';
+            break;
+        case 's': //DOWN
+            map.matrix[line + 1][column] = '@';
+            break;
+        case 'd': //RIGHT
+            map.matrix[line][column + 1] = '@';
+            break;
+    }
+
+    map.matrix[line][column] = '.';
+}
+
+void find_character(int* x, int* y){
+    for(int line = 0; line < map.lines; line++){
+        for(int column = 0; column < map.columns; column++){
+            if(map.matrix[line][column] == '@'){
+                (*x) = line;
+                (*y) = column;
+            }
+        }
+    }
 }
